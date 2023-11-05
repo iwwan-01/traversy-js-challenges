36f93e8b8ca8eff0950a1a220bb52590d53a65f6
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-11-05 18:25:54 +0200

    Completed '03 - High Order Array Methods'

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,21 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNums = numbers.filter((num) => num % 2 === 0);
+
+  const evenNumsSquares = evenNums.map((num) => Math.pow(num, 2));
+
+  const sum = evenNumsSquares.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  return sum;
+}
+
+// function sumOfEvenSquares(numbers) {
+//   const evenSquares = numbers
+//     .filter((num) => num % 2 === 0)
+//     .map((num) => num ** 2)
+//     .reduce((sum, num) => sum + num, 0);
+//   return evenSquares;
+// }
 
 module.exports = sumOfEvenSquares;
