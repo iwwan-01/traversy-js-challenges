6443b06f93058350a2c66f4b297e8d6135462857
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-11-08 23:07:19 +0200

    Completed '04 - Reursion'

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,12 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str === '') {
+    return '';
+  }
+
+  return reverseString(str.substring(1)) + str[0];
+}
+
+// const reverseString = (str) =>
+//   str === '' ? '' : reverseString(str.substring(1)) + str.charAt(0);
 
 module.exports = reverseString;
--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,15 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let result = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result = flattenArray(result.concat(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
 
 module.exports = flattenArray;
